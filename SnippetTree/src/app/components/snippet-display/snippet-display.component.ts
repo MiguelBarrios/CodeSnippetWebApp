89ea61451a8f2eb1a8476ec3383fa389593c7746
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-05-19 14:28:50 -0500

    dynamic directory display complte

--- SnippetTree/src/app/components/snippet-display/snippet-display.component.ts
+++ SnippetTree/src/app/components/snippet-display/snippet-display.component.ts
@@ -18,8 +18,27 @@ export class SnippetDisplayComponent implements OnInit {
   }
 
   loadSelected(pathstr:string){
+
     var path = pathstr.split('-');
-    console.log(path);
+
+    //Brute force
+    var directories = document.getElementsByClassName('directory');
+    for(let i = 0; i < directories.length; ++i){
+      let cur = directories[i];
+      console.log(cur);
+      cur.classList.add('hidden');
+    }
+
+    //show correct directories
+    let fullPath = path[0];
+    document.getElementById(fullPath)?.classList.remove('hidden');
+
+    for(let i = 1; i < path.length; ++i){
+      fullPath = fullPath + '-' + path[i].replace(' ', '_');
+      console.log(fullPath);
+      document.getElementById(fullPath)?.classList.remove('hidden');
+    }
+
 
   }
 
